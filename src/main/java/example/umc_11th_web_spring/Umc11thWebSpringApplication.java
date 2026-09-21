047ea42fd0fe36248c21f0f75f372fa2dea57c33
ChoiMinSang <min393917@gmail.com>
2026-09-21 11:19:42 +0900

    initial commit

---
+++ src/main/java/example/umc_11th_web_spring/Umc11thWebSpringApplication.java
@@ -0,0 +1,13 @@
+package example.umc_11th_web_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc11thWebSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc11thWebSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/umc_11th_web_spring/Umc11thWebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package example.umc_11th_web_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc11thWebSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
